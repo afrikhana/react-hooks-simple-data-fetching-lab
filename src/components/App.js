076a61fc81fadee94ba47baea25a19db95b5594e
Afrikhana <brianmurigi64@gmail.com>
2023-04-23 17:30:14 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, {useEffect, useState} from 'react'
+
+
+function App (){
+   const [image,setImage]=useState([])
+    useEffect(()=>{
+          fetch("https://dog.ceo/api/breeds/image/random")
+          .then((rec)=>rec.json())
+          .then((data)=>{
+            setImage(data.message)
+          })
+        
+    },[]);
+
+
+    return (
+        <div>
+            <p>Loading...
+            </p>
+           
+         <img src={image} key={image} alt='ARandom Dog'></img>
+              
+        </div>
+    );
+}
+
+export default App
